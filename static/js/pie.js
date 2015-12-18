7c2a5b01bef0b093261011b5c58878bc2595af17
Uma Petersen <Uma@Umas-MacBook-Pro.local>
2015-12-18 14:09:02 -0800

    deploying about me page

---
+++ static/js/pie.js
@@ -0,0 +1,22 @@
+
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
+
+        var options = {
+          title: 'My Daily Activities',
+          is3D: true,
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+        chart.draw(data, options);
+      }
+   
