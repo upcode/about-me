83265f3df28bae686c108e53938d607893998edd
Uma Petersen <Uma@Umas-MBP.lan>
2017-05-01 13:52:10 -0700

    moved files to tempalte

--- static/js/nav.js
+++
@@ -1,9 +0,0 @@
-  $('document').ready(function() {
-      	var sourceSwap = function () {
-        var $this = $(this);
-        var newSource = $this.data('alt-src');
-        $this.data('alt-src', $this.attr('src'));
-        $this.attr('src', newSource);
-    }
-
-  });
