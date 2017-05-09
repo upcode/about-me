66aa0ef351d957395d8e6ada258b7d2de8978782
Uma Petersen <Uma@Umas-MacBook-Pro.local>
2017-05-09 02:44:10 -0700

    fixed routes

--- routes.py
+++ routes.py
@@ -3,20 +3,11 @@ import sys
 from flask import Flask, render_template, Response
 from flask import send_file, current_app as app
 from flask import Flask, make_response
-from flask_mail import Mail
+
 ################################################################################
 app = Flask(__name__)
 
-app.config.update(
-    DEBUG=True,
-    #EMAIL SETTINGS
-    MAIL_SERVER='smtp.gmail.com',
-    MAIL_PORT=465,
-    MAIL_USE_SSL=True,
-    MAIL_USERNAME = 'nerdlife1010.com',
-    MAIL_PASSWORD = 'NerdLife!510'
-    )
-mail = Mail(app)
+
 ################################################################################
 app.secret_key = os.environ.get("FLASK_SECRET_KEY", "freedom")
 
@@ -28,14 +19,6 @@ app.secret_key = os.environ.get("FLASK_SECRET_KEY", "freedom")
 def index():
     """ Personal Web Site"""
     return render_template("base_page.html")
-    msg = Message(
-              'Hello',
-           sender='uma.petersen@gmail.com',
-           recipients=
-               ['nerdlife1010.com'])
-    msg.body = "This is the email body"
-    mail.send(msg)
-    return "Sent"
 
     return '''
         <html><body>
@@ -43,10 +26,6 @@ def index():
         </body></html>
         '''
 
-@app.route("/thanks")
-def thanks():
-
-    return render_template("thanks.html")
 
 @app.route("/resume")
 def get_resume():
